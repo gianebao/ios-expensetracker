ef4db13ca6aaf6a42759d16a346a601f7f6638dd
Gian Carlo Val Ebao <gianebao@gmail.com>
2023-11-30 12:00:26 +0800

    Initial Commit

---
+++ expense tracker/expense_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expense_trackerApp.swift
+//  expense tracker
+//
+//  Created by Gian Carlo Val Ebao on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct expense_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
